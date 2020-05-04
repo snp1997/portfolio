3bc79a31c56bad059dc101bb0fed6e6a03c7fffe
sandeep <sandeepezhuthupurakkal@gmail.com>
2020-05-04 14:38:56 +0530

    my portfolio

---
+++ server.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template, url_for, request, redirect
+app = Flask(__name__)
+
+@app.route('/')
+def my_home():
+    return render_template('index.html')
+
+@app.route('/<string:page_name>')
+def html_pages(page_name):
+    return render_template(page_name)
+
+def write_to_file(data):
+	with open('database.txt', mode='a')as database:
+		email = data["email"]
+		subject = data["subject"]
+		message = data["message"]
+		file = database.write(f'\n{email},{subject},{message}')
+
+@app.route('/submit_form', methods=['POST', 'GET'])
+def submit_form():
+    if request.method == 'POST':
+    	try:
+    	    data = request.form.to_dict()
+    	    write_to_file(data)
+    	    return redirect('/thankyou.html')
+    	except:
+    		return 'not saved'
+
+
+
+    else:
+    	return 'something went wrong'
+
